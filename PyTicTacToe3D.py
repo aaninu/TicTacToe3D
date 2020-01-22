46f8c0ebcd46dbf51ed313dc1bb12264dd686ed0
Aninu <aninualin@gmail.com>
2020-01-22 21:49:16 +0200

    Update PyTicTacToe3D.py

--- PyTicTacToe3D.py
+++ PyTicTacToe3D.py
@@ -7,12 +7,17 @@ In aceasta librarie sunt imbinate toate librariile si este afisat jocul pentru a
 Mai multe detalii: http://aninu.xyz/post/tic-tac-toe-3d-python3/
 """
 
-__version__ = '1.2'
+__version__ = '1.3'
 __date__    = '01.01.2018'
 __author__  = 'Anghelina Alin <dev@aninu.ro>'
 
+#Imports
 from Interface import *
 
+# Show IPv4
+Rqm.Get_IPAddress()
+
+# Prepare Game Interface
 Window = Tk()
 Interface(Window)
 Window.mainloop()
